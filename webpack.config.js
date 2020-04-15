54567dc4ff5151435480c461e953ee64c3248b69
snowsword <3068054878@qq.com>
2020-04-15 15:41:08 +0800

    增加html相关插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,45 @@
 const path = require('path');
 
+// 引入HtmlWebpackPlugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 module.exports = {
     // 单个入口文件
     // entry: './src/index.js',
     // 多个入口文件
-    entry:{
-        app:"./src/index.js",
-        print:"./src/print.js"
+    entry: {
+        app: "./src/index.js",
+        print: "./src/print.js"
     },
 
     output: {
         // filename: 'bundle.js',
-        filename:"[name].bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
-    module:{
-        rules:[
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '插件测试',
+            template:"./src/index.html"
+        })
+    ],
+    module: {
+        rules: [
             {
-                test:/\.css$/,
-                use:[
+                test: /\.css$/,
+                use: [
                     "style-loader",
                     "css-loader"
                 ]
             },
             {
-                   test: /\.(png|svg|jpg|gif)$/,
-                      use: [
-                       'file-loader'
-                      ]
-                   }
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
         ]
     }
 };
