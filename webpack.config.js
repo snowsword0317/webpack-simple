1039b57ecb6de213f4685bc86191d3bd9e33e1bb
snowsword <3068054878@qq.com>
2020-04-15 13:35:14 +0800

    有关css打包的

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
 
-console.log("-----------打印配置-------------");
-console.log(path);
-console.log(__dirname);
-console.log(__filename);
-console.log("-----------打印完毕-------------");
-
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
     }
 };
