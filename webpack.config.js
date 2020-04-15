1487fdd2d31f2e86fb5069b0c6f90fb47b233b83
snowsword <3068054878@qq.com>
2020-04-15 14:08:35 +0800

    多个入口文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    // 单个入口文件
+    // entry: './src/index.js',
+    // 多个入口文件
+    entry:{
+        app:"./src/index.js",
+        print:"./src/print.js"
+    },
+
     output: {
-        filename: 'bundle.js',
+        // filename: 'bundle.js',
+        filename:"[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
     module:{
@@ -14,7 +22,13 @@ module.exports = {
                     "style-loader",
                     "css-loader"
                 ]
-            }
+            },
+            {
+                   test: /\.(png|svg|jpg|gif)$/,
+                      use: [
+                       'file-loader'
+                      ]
+                   }
         ]
     }
 };
