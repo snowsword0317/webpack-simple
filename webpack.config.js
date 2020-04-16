3b94091488b2a374ab8e0e2ba49875110118f622
snowsword <3068054878@qq.com>
2020-04-16 10:38:35 +0800

    错误映射到文件

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
